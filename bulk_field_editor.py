5b484f55d1515ac5500b088f40d5bc19c7ef3d2b
tyleragostinobytecode <tyler.agostino@bytecode.io>
2022-04-08 19:24:25 -0400

    initial framework and remove test lookml

--- bulk_field_editor.py
+++ bulk_field_editor.py
@@ -7,11 +7,42 @@ def main(repo_name, github_access_token, source_branch, destination_branch, modi
         repo= repo_name,
         access_token=github_access_token
     )
+
+    modifications = parse_modifications_from_file(modifications_file_path)
+    for view_file in modifications:
+        for field in view_file:
+            for property_to_change, new_value in field:
+                apply_update_to_property(view_file, field, property_to_change, new_value)
     
-    modifications_file_handle = open(modifications_file_path, mode='r')
-    header_row = modifications_file_handle.readline() #just one line
-    modifications = modifications_file_handle.readlines() #the rest of the lines
+def parse_modifications_from_file(filename):
+    pass
     
+def apply_update_to_property(view_file: lookml.File, view_name, field, property_to_change, new_value):
+    field_to_update =  view_file[view_name][field]
+    field_type = field_to_update._type
+    if field_type == lookml.lookml.Dimension:
+        dimension: lookml.lookml.Dimension = field_to_update
+        try:
+            if property_to_change == 'primary_key':
+                if new_value != "yes" and new_value != "no":
+                    raise InvalidDimensionAttributeValue(new_value + " is not a valid value for attribute " + property_to_change) 
+                else:
+                    dimension.primary_key = new_value
+            elif property_to_change == '':
+                pass
+            else:
+                raise InvalidDimensionProperty
+        except Exception as e:
+            print("Failed: " + e)
+
+    elif field_type == lookml.lookml.Measure:
+        measure: lookml.lookml.Measure = field_to_update
+
+class InvalidDimensionAttributeValue(Exception):
+    pass
+    
+class InvalidDimensionProperty(Exception):
+    pass
 
 if __name__ == "__main__":
     # parse arguments
